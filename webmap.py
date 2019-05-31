1f1fe1cd76cc7fe5231c7ba9983ae204e67e9599
AZelinsky96 <azelinsky1996@gmail.com>
2019-05-31 12:25:45 -0400

    Cleaned up the directory and changed html file name

--- webmap.py
+++ webmap.py
@@ -16,46 +16,44 @@ import os
 import webbrowser
 
 def main():
-    
+
     ## Creating the base map object
-    map_ =  folium.Map(location = [40.2171,-74.7429], zoom_start= 5, tiles = 'OpenStreetMap')   
+    map_ =  folium.Map(location = [40.2171,-74.7429], zoom_start= 5, tiles = 'OpenStreetMap')
 
-    
+
     def map_save(map_obj = map_, open_ = True):
-    
-        map_obj.save("webmap.html")
+
+        map_obj.save("webmap1.html")
         ## This will control the browser to open up the html file
-        webbrowser.open_new_tab(os.path.realpath("webmap.html"))
+        webbrowser.open_new_tab(os.path.realpath("webmap1.html"))
 
-    
-    
+
+
     ## Adding children to the map: marker
-    
+
     fg = folium.FeatureGroup(name = "First Markers")
-    fg.add_child(folium.Marker(location = [40.2171, -74.76], popup  = "Trenton!", icon = folium.Icon(color = 'green')))    
-    
-    
-    
-    
+    fg.add_child(folium.Marker(location = [40.2171, -74.76], popup  = "Trenton!", icon = folium.Icon(color = 'green')))
+
+
+
+
     map_.add_child(fg)
-    
 
 
 
 
-    ## Saving and plotting the map    
+
+    ## Saving and plotting the map
     map_save()
-    
-    
-    
+
+
+
     return None
 
 
 
 
 
-if __name__ == "__main__": 
-    
+if __name__ == "__main__":
+
     main()
-
-
