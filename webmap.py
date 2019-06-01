08d701ec5292251e09f7bc346314e8ed72245314
AZelinsky96 <azelinsky1996@gmail.com>
2019-05-31 23:33:28 -0400

    Update: 5_31

--- webmap.py
+++ webmap.py
@@ -15,6 +15,54 @@ import folium
 import os
 import webbrowser
 
+import requests
+from bs4 import BeautifulSoup as bs
+
+def scraper():
+    """
+    I am creating this function to scrape the data off of the specified webpage. I am keeping this out of main however,
+
+    and calling it if only necessary.
+    """
+
+    ## Pulling in the data from the url.
+    url = requests.get("https://inkplant.com/code/state-latitudes-longitudes")
+    #Utilizing Beautiful soup to retrieve the page content
+    soup = bs(url.content, "html.parser")
+    # Finding all of the table data tags
+    table_data = soup.find_all("td")
+
+    ## Finding the column names
+    headers = [i.get_text().strip() for i in table_data[:3]]
+    #print(headers)
+    state = list()
+    lat   = list()
+    long  = list()
+
+    ## Utilizing this dictionary to append the data to the associated list based off of counter
+    appender = {
+        1 : state,
+        2 : lat,
+        3 : long
+    }
+
+    ## For loop designed to aggregate data to appropriate list
+    counter = 1
+    for i in table_data[3:]:
+        appender[counter].append(i.get_text())
+        counter += 1
+        if counter == 4:
+            counter = 1
+
+    ## Creating a dataframe out of the list
+    df = pd.DataFrame(columns = headers)
+    df[headers[0]] = state
+    df[headers[1]] = lat
+    df[headers[2]] = long
+
+    df.to_csv("State_Lat_Long.csv")
+    return df
+
 def main():
 
     ## Creating the base map object
@@ -25,7 +73,8 @@ def main():
 
         map_obj.save("webmap1.html")
         ## This will control the browser to open up the html file
-        webbrowser.open_new_tab(os.path.realpath("webmap1.html"))
+        if open_ == True:
+            webbrowser.open_new_tab(os.path.realpath("webmap1.html"))
 
 
 
@@ -34,17 +83,48 @@ def main():
     fg = folium.FeatureGroup(name = "First Markers")
     fg.add_child(folium.Marker(location = [40.2171, -74.76], popup  = "Trenton!", icon = folium.Icon(color = 'green')))
 
-
-
-
     map_.add_child(fg)
+    ## Adding multiple markers
+
+    fg1 = folium.FeatureGroup(name = "MultiMark")
+
+    ## Reading in the state lat long dataframe
+    try:
+        ## If the file does not exist in directory
+        df = pd.read_csv("State_Lat_Long.csv")
+    except:
+        ## The function will be called to recall the source data and create the file
+        print("File not in Directory.")
+        df = scraper()
+        print("File Created and loaded")
+
+    for i,k in df.iterrows():
+        fg1.add_child(folium.Marker(location = [k[2], k[3]], popup = "{}".format(k[1]), icon = folium.Icon(color = 'blue')))
+
+    map_.add_child(fg1)
+
+    state_df = pd.read_json("us_state_capitals.json", 'index')
+
+    fg2 = folium.FeatureGroup(name = 'StateCapitals')
+    for i, k in state_df.iterrows():
+        fg2.add_child(folium.Marker(location = [k[1], k[2]], popup = "{}".format(k[0]), icon = folium.Icon(color = 'red')))
+
+    map_.add_child(fg2)
+    #print(df.head())
+
+
+
 
 
 
 
 
     ## Saving and plotting the map
-    map_save()
+    open_map = input("Open map? [y/n] ")
+    if open_map.lower() == 'y':
+        open_map = True
+    else: open_map = False
+    map_save(open_ = open_map)
 
 
 
