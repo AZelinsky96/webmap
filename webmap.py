b07581a08d849d279dfcc0a0ea8586c0362d0c99
AZelinsky96 <azelinsky1996@gmail.com>
2019-06-01 00:54:18 -0400

    Added volcanoes to the map, and added some stylistic changes

--- webmap.py
+++ webmap.py
@@ -103,16 +103,54 @@ def main():
 
     map_.add_child(fg1)
 
+    ## Reading in the state capital data, and appending it to the web map
     state_df = pd.read_json("us_state_capitals.json", 'index')
-
+    ## Creating a group to add the next batch of data into
     fg2 = folium.FeatureGroup(name = 'StateCapitals')
+    ## Iterating over the rows in dataframe and appending it to the map
     for i, k in state_df.iterrows():
-        fg2.add_child(folium.Marker(location = [k[1], k[2]], popup = "{}".format(k[0]), icon = folium.Icon(color = 'red')))
-
+        fg2.add_child(folium.Marker(location = [k[1], k[2]], popup = "{}".format(k[0]), icon = folium.Icon(color = 'green')))
+    ## Adding all of the edits to the file
     map_.add_child(fg2)
-    #print(df.head())
+
+    ## Playing around with volcano data.
+    fg3 = folium.FeatureGroup(name = "Volcanoes")
+    with open("Volcanoes.txt", "r") as f:
+        ## Rearding a list of lines from the stream above
+        contents = f.readlines()
+        #[print(i) for i in contents[:5]]
+
+    df_volcanoes = pd.read_csv("Volcanoes.txt")
+
+    ## Calculating the range and 3 steps for the elevation values
+    ## This will be used to bin the volcanoes into 3 categories
+    min_   = min(df_volcanoes['ELEV'])
+    max_   =  max(df_volcanoes["ELEV"])
+    range_ = max_ - min_
+    step_  = range_ / 3
+    step1  = min_ + step_
+    step2  = step1 + step_
+    step3  = step2 + step_
+
+    def step_finder(x, step1, step2, step3):
+        """
+        This function will return colors based on where the value x falls in relation to the 3 bins above
+        """
+        if x <= step1:
+            return "pink"
+        elif (x <= step2) and (x > step1):
+            return "orange"
+        else:
+            return "red"
 
 
+    for i, k in df_volcanoes.iterrows():
+        color = step_finder(float(k[5]), step1, step2, step3)
+        fg3.add_child(folium.Marker(location = [k[8], k[9]],
+                                    popup = "Volcano: {} \nStatus: {} \nElevation: {}".format(k[2], k[4], k[5]),
+                                    icon = folium.Icon(color = color)))
+
+    map_.add_child(fg3)
 
 
 
